84f39f585b694d9a2c078b7e6ca13e4d358577cc
Sunny Kevin <649778671@qq.com>
2018-10-15 02:20:55 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidArgumentException.php
+++ src/Exceptions/InvalidArgumentException.php
@@ -1,8 +1,16 @@
 <?php
 
+/*
+ * This file is part of the sunny-kevin/weather.
+ *
+ * (c) sunnykevin <i@sunnykevin.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace SunnyKevin\Weather\Exceptions;
 
 class InvalidArgumentException extends Exception
 {
-
 }
--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the sunny-kevin/weather.
+ *
+ * (c) sunnykevin <i@sunnykevin.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace SunnyKevin\Weather\Tests;
 
 use GuzzleHttp\Client;
